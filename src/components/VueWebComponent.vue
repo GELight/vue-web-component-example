5a17d0e9fcf1e2ffa808a174f6a3a8ec4dc080c6
Mario Linz <gelight@gmx.de>
2019-05-19 22:42:25 +0200

    ...

--- src/components/VueWebComponent.vue
+++ src/components/VueWebComponent.vue
@@ -17,29 +17,16 @@ export default {
     // } else {
     //   return h(this.load('www.google.de'))
     // }
-    return h(this.load('www.google.de'))
+    return h(this.load('http://www.google.de'))
   },
   methods: {
     load (url) {
-      return fetch(url)
-        .then(response => {
-          return response.text()
-        })
-        .then(html => {
-          // Initialize the DOM parser
-          var parser = new DOMParser()
-
-          // Parse the text
-          var doc = parser.parseFromString(html, 'text/html')
-          // You can now even select part of that html as you would in the regular DOM
-          // Example:
-          // var docArticle = doc.querySelector('article').innerHTML
-          console.log(doc)
-          return doc
-        })
-        .catch(err => {
-          console.log('Failed to fetch page: ', err)
-        })
+      return fetch(url, {
+        mode: 'no-cors',
+        method: 'get'
+      }).then(response => {
+        return response.text()
+      })
     }
   }
 }
